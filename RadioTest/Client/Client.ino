6615e9cdccdd2c11cdfac63549861bd449025fa7
stannive49 <72883060+stannive49@users.noreply.github.com>
2021-02-14 14:22:17 -0500

    Problem 4

--- RadioTest/Client/Client.ino
+++ RadioTest/Client/Client.ino
@@ -1,4 +1,4 @@
-//*************************************************************
+/*************************************************************
   This is an examples for the RL01-02-03 Radio Range
 
   You can buy one on our store!
@@ -15,51 +15,57 @@
 
   The sensor communicates over the I2C Bus.
 
+  ------------------------TIPS--------------------------
+  Change this line ----->Wire.begin(2,14);
+  to this      ----->Wire.begin();
+  to allow this sensor to communicate other cpus
+
 *************************************************************/
 
 #include <xCore.h>
 #include <xRL0x.h>
 
 #define RL03_FREQ 915.0
+#define CW01_RED 12
+#define CW01_GREEN 13
+#define CW01_BLUE 5
+#define Serial SerialUSB
 
 void setup() {
-   // Start the I2C Comunication
-   #ifdef ESP8266
-    Wire.pins(2, 14);
-    Wire.setClockStretchLimit(15000);
-  #endif
-  Wire.begin();
-
   // Start the Serial Monitor
-  SerialUSB.begin(115200);
-  // Set the RGB Pin directions
-  //pinMode(LED_RED, OUTPUT);
-  //pinMode(LED_GREEN, OUTPUT);
-  //pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 
-  if (!RL0X.begin()) {
-    SerialUSB.println("Check the connector to CW01");
+  // Set the RGB Pin directions
+  pinMode(CW01_RED, OUTPUT);
+  pinMode(CW01_GREEN, OUTPUT);
+  pinMode(CW01_BLUE, OUTPUT);
+
+  // Start the I2C Comunication
+  Wire.pins(2, 14);
+  Wire.begin();
+  Wire.setClockStretchLimit(15000);
+
+  if (!RL0X.begin()) { // <-- enter radio name here
+    Serial.println("Check the connector to CS11");
     while (1) {
       // Flash RED to indicate failure
-      SerialUSB.print("Not connecting to CW01");
-      digitalWrite(LED_RED, HIGH);
+      digitalWrite(CW01_RED, HIGH);
       delay(100);
-      digitalWrite(LED_RED, LOW);
+      digitalWrite(CW01_RED, LOW);
       delay(100);
     }
   } else {
     // RL0X Initialized correctly
-    SerialUSB.print("Connected to CW01");
-    RL0X.setModemConfig(RL0X.Bw31_25Cr48Sf512);
+    RL0X.setModemConfig(RL01.Bw31_25Cr48Sf512);
     RL0X.setFrequency(RL03_FREQ);
     RL0X.setTxPower(23, false);
   }
 }
 
 void loop() {
-  SerialUSB.println("Sending to RL0X Server");
+  Serial.println("Sending to RL0X Server");
 
-  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(CW01_GREEN, HIGH);
 
   uint8_t data[] = "Hello World!";
   delay(100);
@@ -68,17 +74,17 @@ void loop() {
   uint8_t buf[195];
   uint8_t len = sizeof(buf);
 
-  if (RL0X.waitAvailableTimeout(3000)) {
+  //if (RL0X.waitAvailableTimeout(3000)) {
     if (RL0X.recv(buf, &len)) {
-      SerialUSB.print("got reply: ");
-      SerialUSB.println((char*)buf);
-      SerialUSB.print("RSSI: ");
-      SerialUSB.println(RL0X.lastRssi(), DEC);
+      Serial.print("got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(RL0X.lastRssi(), DEC);
     } else {
-      SerialUSB.println("recv failed");
+      Serial.println("recv failed");
     }
-  } else {
-    digitalWrite(LED_GREEN, LOW);
-    SerialUSB.println("No reply, is the RL03 server running ?");
-  }
+//  } else {
+    //digitalWrite(CW01_GREEN, LOW);
+    //Serial.println("No reply, is the RL01 server running ?");
+//  }
 }
